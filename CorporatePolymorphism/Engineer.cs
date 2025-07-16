23665a1b74e5f3a4ae4ea3087da7b90026f2fdce
Flavius <flavius09@protonmail.com>
2025-07-16 13:28:28 +0200

    Create Engineer.cs

---
+++ CorporatePolymorphism/Engineer.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CorporatePolymorphism
+{
+  // Placeholder for Engineer class implementation
+  public class Engineer : Employee
+  {
+    public override void SubmitDailyReport(){
+      Console.WriteLine("Engineer works on a project.");
+    }
+
+    public override void Work(){
+      Console.WriteLine("Engineer works on a project");
+    }
+    
+  }
+}
